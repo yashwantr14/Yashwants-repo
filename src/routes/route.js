da638dc86b5048602bd0527c624e921641091af5
yashwantr14 <yashwantrajputt@gmail.com>
2022-10-19 20:34:09 +0530

    Assignment on Missing Number

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,39 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
-let players=[
-    {
-        "name": "manish",
-        "dob": "14/10/1995",
-        "gender": "male",
-        "city": "jalandhar",
-        "sports": [
-            "swimming"
-        ]
-    },
-    {
-        "name": "gopal",
-        "dob": "14/10/1995",
-        "gender": "male",
-        "city": "delhi",
-        "sports": [
-            "soccer"
-        ]
-    },  {
-        "name": "lokesh",
-        "dob": "14/10/1990",
-        "gender": "male",
-        "city": "mumbai",
-        "sports": [
-            "soccer"
-        ]
-    }
-]
-
-router.post('/players',function(req,res){
-    let body=req.body
-    players.push(body)
-    console.log(body)
-    res.send({data: players , status : true})
+router.get('/sol1',function(req,res){
+    
+const arr=[1,2,3,5,6,7,8,9]
+let n=arr[arr.length-1]
+let sum1=n*(n+1)/2
+let sum2=0
+for(let i=0; i<arr.length; i++){
+    sum2+=arr[i]
+}
+let missingNumber=sum1-sum2
+console.log(missingNumber)
+res.send("The missing number is : " + missingNumber)
 })
+
 module.exports = router;
