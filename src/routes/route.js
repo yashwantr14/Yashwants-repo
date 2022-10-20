8366233a0321865309078ba332ce7c704cbb27ee
yashwantr14 <yashwantrajputt@gmail.com>
2022-10-20 20:22:53 +0530

    Assignment of Voting Age

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,17 +1,91 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/sol2',function(req,res){
-    
-const arr=[33,34,35,36,37,38,40]
-let sum1=arr.reduce((x,y)=>x+y)
-let n=arr.length+1
-let first=arr[0]
-let last=arr[arr.length-1]
-let sum2=n*(first+last)/2 // (n+1)*(first+last)/2
-let missingNumber=sum2-sum1
 
-res.send("The missing number is : " + missingNumber)
+/*ASSIGNMENT:
+you will be given an array of persons ( i.e an array of objects )..each person will have  a {name: String , age: Number, votingStatus: true/false(Boolean)}
+take input in query param as votingAge..and for all the people above that age, change votingStatus as true
+also return an array consisting of only the person that can vote
+
+WRITE A POST API TO THE ABOVE
+
+
+take this as sample for array of persons:
+let persons= [
+   {
+   name: "PK",
+   age: 10,
+   votingStatus: false
+},
+{
+   name: "SK",
+   age: 20,
+   votingStatus: false
+},
+{
+   name: "AA",
+   age: 70,
+   votingStatus: false
+},
+{
+   name: "SC",
+   age: 5,
+   votingStatus: false
+},
+{
+   name: "HO",
+   age: 40,
+   votingStatus: false
+}
+]*/
+
+let persons = [
+    {
+        name: "PK",
+        age: 10,
+        votingStatus: false
+    },
+    {
+        name: "SK",
+        age: 20,
+        votingStatus: false
+    },
+    {
+        name: "AA",
+        age: 70,
+        votingStatus: false
+    },
+    {
+        name: "SC",
+        age: 5,
+        votingStatus: false
+    },
+    {
+        name: "HO",
+        age: 41,
+        votingStatus: false
+    }
+]
+
+//for loop approach
+router.post('/votingAge', function (req, res) {
+    let votingAge = req.query.votingAge
+    let persons2 = []
+    for (let i=0; i<persons.length; i++) {
+
+        if (persons[i].age >= votingAge) {
+            persons[i].votingStatus = "true"
+            persons2.push(persons[i])
+        }
+    }
+    res.send({ Persons_who_can_vote: persons2})
 })
 
+//without for loop
+router.post('/votingAge1', function (req, res) {
+    let votingAge = req.query.votingAge
+    let persons3=persons.filter(x=>x.age>=votingAge)
+    persons3.map(x=>x.votingStatus="true")
+    res.send({ Persons_who_can_vote: persons3})
+})
 module.exports = router;
